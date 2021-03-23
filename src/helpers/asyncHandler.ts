ae34f0a13a54a06b50b1041c08a4b01edfd71f23
lhasson <lhasson@liveperson.com>
2021-03-23 13:44:31 +0200

    add file buffer read stream implementation

--- src/helpers/asyncHandler.ts
+++ src/helpers/asyncHandler.ts
@@ -1,10 +1,20 @@
 import {Request} from "express";
 import { StringRequestHandler } from './stringHandler';
+import { FileHandler } from './fileHandler';
+import { ExternalApiHandler } from './externalApiHandler'
 
 const stringRequestHandler = new StringRequestHandler();
+const fileHandler = new FileHandler();
+const externalApiHandler = new ExternalApiHandler();
 
 function asyncHandler(req: Request): Promise<string> {
-    stringRequestHandler.handle(req);
+    if (req.body.type === 1){
+        stringRequestHandler.handle(req);
+    } else if (req.body.type === 2){
+        fileHandler.handle(req);
+    } else if (req.body.type === 3){
+        externalApiHandler.handle(req);
+    }
     return new Promise((resolve) => {
         setTimeout(() => {
             return resolve('test');
--- src/helpers/fileHandler.ts
+++ src/helpers/fileHandler.ts
@@ -1,5 +1,6 @@
 import {Request} from "express";
 import {IWordsResult, RequestHandler} from './requestHandler';
+import fs from 'fs';
 
 export class FileHandler extends RequestHandler {
     private text: string;
@@ -9,6 +10,22 @@ export class FileHandler extends RequestHandler {
     }
 
     public handle(req: Request): IWordsResult{
+        const read = fs.createReadStream('/Users/lhasson/Documents/projects/perfect_node_project/examples/sample.txt', 'utf8');
+        const data = [];
+        read.on('data', function(chunk){
+            console.log('new chunk received');
+            console.log('data :', chunk, chunk.length);
+        });
+
+        read.on('end', () => {
+            console.log('end :', Buffer.concat(data).toString());
+            // end : I am transferring in bytes by bytes called chunk
+        })
+
+        read.on('error', (err) => {
+            console.log('error :', err)
+        })
+
         this.text = req.body.text || "";
         const res = {} as IWordsResult;
         res.lior = 3;
